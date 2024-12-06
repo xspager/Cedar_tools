2eaed22699b58e3a58975ab0c7e98c1415b24cc9
Daniel Lemos <xspager@gmail.com>
2024-12-05 22:33:38 -0300

    Correctly print all 127 ASCII charactes from the font in "ASCII art"

--- print_al_font.c
+++ print_al_font.c
@@ -22,6 +22,7 @@
  * if XW <= 16 char width = (XW-1)/2
  * else XW is code for the extension charcter multiplied by 2
  * the pointers point to the computer word in XHdata for XW
+ * this pointers needed to be added to the numeric value of the ASCII character and the two word offset for the header then multiplied by two to get the right offset in bytes.
  *
  */
 
@@ -47,7 +48,7 @@ struct AL {
     unsigned int baseline : 7;
     uint8_t maxWidth;
     uint16_t pointers[0377]; /* 255 in octal */
-    //struct XHdata chars[255];
+    struct XHdata chars[255];
 }__attribute__((packed));
 
 
@@ -75,6 +76,7 @@ int main(int argc, char *argv[])
     assert(file_size > sizeof(struct AL));
 
     struct AL al;
+    assert(sizeof(struct XHdata) == 4);
 
     int bytes_read = read(fd, &al, sizeof(al));
     assert(bytes_read == sizeof(al));
@@ -84,10 +86,12 @@ int main(int argc, char *argv[])
     printf("Baseline: %i\n", al.baseline);
     printf("maxWidth: %i\n", al.maxWidth);
 
+#if 0
     // print the "self-relative pointers" table
     for (int i = 1; i < '!'+20; i++) {
         printf("pointer %c => 0x%X +256 = 0x%X\n", i, swap_uint16(al.pointers[i]), ((uint8_t)swap_uint16(al.pointers[i]) * 2)+ 256 - ('0'*3));
     }
+#endif
 
     struct XHdata charData;
     char buffer[17];
@@ -95,55 +99,38 @@ int main(int argc, char *argv[])
     // Dummy XW = 1, XH = 0, HD = 0 at
     // 0x203: 0x01 0x00, 0x00 0x00
 
-    for (int i = '#'; i < 255; i++) {
+    for (int i = 0; i < 128; i++) {
         //printf("%X offset %d\n", i, al.pointers[i]);
 
         uint16_t charData_offset = swap_uint16(al.pointers[i]);
-        printf("offset = %c => 0x%x\n", i, charData_offset);
+        // pointer + char * 2 + header offset
+        int real_offset = (charData_offset + i + 2)*2;
+        printf("offset = %d => 0x%x => 0x%X\n", i, charData_offset, real_offset);
 
-        // seek the file to after the header and pointers
-        //off_t cur_offset = lseek(fd, charData_offset+0x202, SEEK_SET);
-        off_t cur_offset = lseek(fd, 0x206 + charData_offset, SEEK_SET);
+        // seek the file to XHdata for this char
+        off_t cur_offset = lseek(fd, real_offset, SEEK_SET);
         assert(cur_offset != -1);
 
         printf("ASCII char '%c'\n", i);
-        //printf("offset = %ld\n", charData_offset);
-        //printf("cur offset = %ld\n", cur_offset);
-
         bytes_read = read(fd, &charData, sizeof(struct XHdata));
 
-        uint16_t scanlines[16];
-        cur_offset = lseek(fd, 0x206 + charData_offset - charData.XH, SEEK_SET);
+        uint16_t scanlines[16] = {0};
+        // go back in file XH words
+        cur_offset = lseek(fd, real_offset - (charData.XH * 2), SEEK_SET);
 
         bytes_read = read(fd, scanlines, charData.XH * 2);
 
+        // FIXME: We need to put the wider charactes toguether
+        // FIXME: We need to handle the omited all zeroes scanlines
         for (int i = 0; i < charData.XH; i++) {
-            sprintf(buffer, "%016b", scanlines[i]);
+            sprintf(buffer, "%016b", swap_uint16(scanlines[i]));
             replace(buffer, '0', ' ');
             replace(buffer, '1', '#');
             printf("%s\n", buffer);
-            //printf("%016b\n", charData.scanlines[i]);
         }
-        printf("XW = %d\n", swap_uint16(charData.XW));
-        printf("HD = %d\n", charData.HD);
-        printf("XH = %d\n", charData.XH);
+
+        printf("XW = %d HD = %d XH = %d\n", swap_uint16(charData.XW), charData.HD, charData.XH);
     }
-    //assert(sizeof(struct XHdata) == 2*16+4);
-
-#if 0
-
-    //                       scanlines + three words of 2 bytes
-    size_t charData_size = (al.height * 2) + 3 * 2;
-
-    uint8_t buffer[255] = {0};
-
-    bytes_read = read(fd, buffer, sizeof(buffer));
-
-    for(int i = 0; i < 50; i=i+2) {
-        printf("%08b%08b\n", buffer[i], buffer[i+1]);
-    }
-#endif
-    //assert(bytes_read == sizeof(struct XHdata) + (sizeof(struct Scanline) * al.height));
 
     return EXIT_SUCCESS;
 }
